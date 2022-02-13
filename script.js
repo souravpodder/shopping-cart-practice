cecd72ad43ff53e76dad35d74ac00dc06d4de5fc
spodder <spodder37@gmail.com>
2022-02-13 20:15:59 +0600

    completed

--- script.js
+++ script.js
@@ -14,11 +14,29 @@ function updateProductNumber(product, isIncreasing, productPrice){
   const productPriceTotal = productPrice * productNumber;
   // show each product price total 
   document.getElementById(product + '-price').innerText = productPriceTotal;
+
+  calculateTotal();
 }
 
+function calculateTotal(){
+  // get pricetotal of each product 
+  const phonePriceTotalText = document.getElementById('phone-price').innerText;
+  const casePriceTotalText = document.getElementById('case-price').innerText;
+
+  const subTotal = parseInt(phonePriceTotalText) + parseInt(casePriceTotalText);
+  const tax = subTotal * 0.1;
+  const total = subTotal + tax;
+
+  document.getElementById('sub-total').innerText = subTotal;
+  document.getElementById('tax').innerText = tax.toFixed(2);
+  document.getElementById('total-amount').innerText = total;
+
+}
 // handle case events 
 document.getElementById('case-plus').addEventListener('click',function(){
+
   updateProductNumber('case', true, 59);
+  
 });
 
 document.getElementById('case-minus').addEventListener('click', function(){
